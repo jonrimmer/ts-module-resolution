76278c0f7f738a802dfd73c58cb714ffcbdbc8ee
Jon Rimmer <jon.rimmer@gmail.com>
2018-04-19 16:18:29 +0100

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var leftPad = require("left-pad");
+var dep_1 = require("sub/dep");
+console.log(leftPad(dep_1.VALUE, 5));
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import * as leftPad from 'left-pad';
+import { VALUE } from 'sub/dep';
+
+console.log(leftPad(VALUE, 5));
